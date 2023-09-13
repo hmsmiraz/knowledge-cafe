96292523ff99f07f982b72f7604e1d0a336578b0
hmsmiraz <hmsmiraz64729@gmail.com>
2023-09-14 00:55:24 +0600

    SECOND

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl font-extrabold p-10 text-center' >Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
